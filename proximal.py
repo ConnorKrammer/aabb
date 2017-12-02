2b033dbfa0e4685f2d0436abf0dd175e527db2df
Connor Krammer <connork99@gmail.com>
2017-12-02 04:03:33 -0700

    Optimize program and tidy code

--- proximal.py
+++ proximal.py
@@ -10,11 +10,18 @@ def get_zonecode(x, y, xmin, ymin, xmax, ymax):
     Return a bit code indicating point's position relative to rect.
 
     """
-    code = INSIDE
-    if   x < xmin: code |= LEFT
-    elif x > xmax: code |= RIGHT
-    if   y < ymin: code |= BOTTOM
-    elif y > ymax: code |= TOP
+    if x < xmin:
+        code = LEFT
+    elif x > xmax:
+        code = RIGHT
+    else:
+        code = INSIDE
+
+    if y < ymin:
+        code |= BOTTOM
+    elif y > ymax:
+        code |= TOP
+
     return code
 
 def clamp(x, xmin, xmax):
@@ -40,6 +47,7 @@ def get_closest(x0, y0, x1, y1, xmin, ymin, xmax, ymax):
 
     """
     # Line segment is horizontal or vertical
+    # Beware floating point precision here
     if x0 == x1 or y0 == y1:
         x0, x1 = clamp_axis(x0, x1, xmin, xmax)
         y0, y1 = clamp_axis(y0, y1, ymin, ymax)
@@ -48,57 +56,53 @@ def get_closest(x0, y0, x1, y1, xmin, ymin, xmax, ymax):
     p0_code = get_zonecode(x0, y0, xmin, ymin, xmax, ymax)
     p1_code = get_zonecode(x1, y1, xmin, ymin, xmax, ymax)
 
-    slope = (y0 - y1) / (x0 - x1)
-    intercept = y0 - x0 * slope
-
     while (True):
         # Line segment is entirely within rect
         if p0_code == p1_code == INSIDE:
             return x0, y0, x1, y1
 
-        # Line segment is on just one side of rect
+        # Equals zero if endpoints don't share a side
         code = p0_code & p1_code
+
+        # Line segment is on just one side of rect
         if code:
-            if code & BOTTOM:
-                y = ymin
-            elif code & TOP:
-                y = ymax
-            elif code & LEFT:
+            if code & LEFT:
+                x = xmin
                 y = y0 if x0 > x1 else y1
                 y = clamp(y, ymin, ymax)
             elif code & RIGHT:
+                x = xmax
                 y = y0 if x0 < x1 else y1
                 y = clamp(y, ymin, ymax)
-
-            if code & LEFT:
-                x = xmin
-            elif code & RIGHT:
-                x = xmax
             elif code & BOTTOM:
+                y = ymin
                 x = x0 if y0 > y1 else x1
                 x = clamp(x, xmin, xmax)
             elif code & TOP:
+                y = ymax
                 x = x0 if y0 < y1 else x1
                 x = clamp(x, xmin, xmax)
 
             return x, y
 
-        # Ambiguous case; clip line segement and recheck on next iteration
+        # Pick the first non-interior endpoint
         code = p0_code or p1_code
+
+        # Ambiguous case; clip line segement and recheck on next iteration
         if code & LEFT:
             x = xmin
-            y = slope * x + intercept
+            y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0);
         elif code & RIGHT:
             x = xmax
-            y = slope * x + intercept
-        elif code & TOP:
-            y = ymax
-            x = (y - intercept) / slope
+            y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0);
         elif code & BOTTOM:
             y = ymin
-            x = (y - intercept) / slope
+            x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0)
+        elif code & TOP:
+            y = ymax
+            x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0)
 
-        # Update endpoint and zone code
+        # Update clipped endpoint
         if code == p0_code:
             x0, y0 = x, y
             p0_code = get_zonecode(x0, y0, xmin, ymin, xmax, ymax)
